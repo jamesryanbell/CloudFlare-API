a5c8870c1048d3a670e7354a8ec7a9b28f3cce48
Oriental Sensation <github@orientalsensation.com>
2013-09-25 19:00:24 +0200

    $data class entity is never used since each method has its own local $data variable.

--- class_cloudflare.php
+++ class_cloudflare.php
@@ -30,9 +30,6 @@ class cloudflare_api {
     //Stores the email login
     private $email;
 
-    //Data to post
-    private $data = array();
-
     /**
      * Make a new instance of the API client
      */
